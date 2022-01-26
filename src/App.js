ff166e9513a6bb21c5eb0589c4366e1481000f1a
akarshshri <akarshshrivastava97@gmail.com>
2022-01-26 17:21:15 +0530

    Api functionality done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import LoggedIn from './components/LoggedIn';
+
+const api = axios.create(
+  { baseURL: `https://myphysio.digitaldarwin.in/api/login/` }
+)
+
+const Stateless = () => {
+  if(localStorage.getItem('apiinfo') === null){
+    localStorage.setItem('apiinfo',JSON.stringify([{usename: '', password: '' , userState: '',name:''}]));
+  }
+};
 
 function App() {
+
+  const userchangeHandle = (event)=>{
+    setUsername(event.target.value);
+  }
+  const passwordchangeHandle = (event)=>{
+    setPassword(event.target.value);
+  }
+
+  const [status, setStatus] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [res, setRes] = useState(' ');
+  useEffect(() => {
+  }, []);
+
+  const logout = ()=>{
+    setRes('');
+    let arr = [{usename: '', password: '' , userState: ''}]
+    localStorage.removeItem('apiinfo');
+    localStorage.setItem('apiinfo',JSON.stringify(arr));
+   
+  }
+
+  const submitEvent = async (e)=>{
+    e.preventDefault()
+    let temp = await api.post('/', { "uid": username, "password": password, "blocked": 0 }) 
+    setRes(temp)
+    setStatus('OK')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      {Stateless()}
+      {(res.statusText === 'OK' || JSON.parse(localStorage.getItem('apiinfo'))[0].userState === 'OK')?(
+        <LoggedIn response ={ res} userState={res.statusText} logout={logout} username={username} password={password} status={status}/>
+      ): (
+        <form onSubmit={submitEvent}><h1>Login</h1>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label" >Username</label>
+          <input type="text" className="form-control" id="username" aria-describedby="emailHelp" placeholder='maymis10098' onChange={userchangeHandle}/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" placeholder='Mayank@123' onChange={passwordchangeHandle}/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      )}
+      
     </div>
   );
 }
---
+++ src/components/LoggedIn.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+var tempArr = [];
+
+const updateTable = () => {
+
+    let s = '';
+    tempArr.forEach((element, index) => {
+        s += `<tr>
+        <th>${tempArr[index].patient_code}</th>
+      <td scope="row">${element.first_name}  ${element.last_name}</td>
+      <td scope="row">${element.dob}</td>
+      <td scope="row">${element.mobile_no}</td>
+       </tr>
+       `
+    });
+    document.getElementById('tableBody').innerHTML = s;
+}
+
+function LoggedIn({ logout, userState, username, password, status, res }) {
+
+    if (status === 'OK') {
+        let arr = [{ usename: username, password: password, userState: userState }]
+        localStorage.setItem('apiinfo', JSON.stringify(arr)) //Not Recommended to Store Password in LocalStorage
+    }
+
+    const Table = async () => {
+
+        const res = await axios.post('https://myphysio.digitaldarwin.in/api/get-patient/', { "id": 1 }, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+                //Cookie: csrftoken=q0FqcIRO1do5fs8c7A5xOA0tq9PwYicn24k7Ajv7AX0oVn9FUgUwI7WLtUwXxVXE; sessionid=wjtbes2w68vagjm123nj7jreughzpzhf; jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.bdfW8B6lG7RhPmHCtO6rPgf3IYlDwAJc7LUKtfTE2eU; tran=satmis1000003
+                //JS does not allow to state cookie in header to prevent unauthorized use 
+            }
+        })
+        res.data.forEach(element => {
+            tempArr.push(element);
+        })
+        updateTable();
+    }
+
+
+
+useEffect(() => {
+        Table();
+
+}, []);
+
+return <div>
+    <div className="row grid" style={{marginTop: '1rem'}}>
+    <div className='col'><h1>Welcome</h1></div>
+    <div className='col'><button className="btn btn-danger" onClick={logout}>Logout</button></div>
+    </div>
+    <div className='container d-flex row m-auto my-4' id='patinfo' style={{ border: 'grey medium solid' }}>
+        <h3>Customer's Info</h3>
+        <table className="table"  >
+            <thead>
+                <tr>
+                    <th scope="col">Code</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">DOB</th>
+                    <th scope="col">Contact</th>
+                </tr>
+            </thead>
+            <tbody id="tableBody">
+                    
+                
+            </tbody>
+        </table>
+    </div>
+</div>;
+}
+
+export default LoggedIn;
+
+
